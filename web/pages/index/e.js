97e9e8eaa41be6886a2787906593a52e1dbc0fe4
mer <iwangyang@qq.com>
2018-01-31 16:15:48 +0800

    e.js

--- web/pages/index/e.js
+++ web/pages/index/e.js
@@ -51,3 +51,113 @@ sign.find('.password').on('keydown', function(e) {
 sign.find('input').on('focus', function(e) {
     sign.find('.tips').fadeOut(null, null, true)
 })
+
+let options = {
+    "particles": {
+        "number": {
+            "value": 120,
+            "density": {
+                "enable": true,
+                "value_area": 800
+            }
+        },
+        "color": {
+            "value": "#ffffff"
+        },
+        "shape": {
+            "type": "circle",
+            "stroke": {
+                "width": 0,
+                "color": "#000000"
+            },
+            "polygon": {
+                "nb_sides": 10
+            },
+            // "image": {
+            //     "src": "img/github.svg",
+            //     "width": 100,
+            //     "height": 100
+            // }
+        },
+        "opacity": {
+            "value": 0.5,
+            "random": true,
+            "anim": {
+                "enable": false,
+                "speed": 1,
+                "opacity_min": 0.1,
+                "sync": false
+            }
+        },
+        "size": {
+            "value": 6,
+            "random": true,
+            "anim": {
+                "enable": true,
+                "speed": 20,
+                "size_min": 0.1,
+                "sync": false
+            }
+        },
+        "line_linked": {
+            "enable": true,
+            "distance": 200,
+            "color": "#ffffff",
+            "opacity": 0.6,
+            "width": 1
+        },
+        "move": {
+            "enable": true,
+            "speed": 1,
+            "direction": "none",
+            "random": false,
+            "straight": false,
+            "out_mode": "out",
+            "attract": {
+                "enable": false,
+                "rotateX": 600,
+                "rotateY": 1200
+            }
+        }
+    },
+    "interactivity": {
+        "detect_on": "canvas",
+        "events": {
+            "onhover": {
+                "enable": true,
+                "mode": ["grab", "bubble"]
+            },
+            "onclick": {
+                "enable": true,
+                "mode": "repulse"
+            },
+            "resize": true
+        },
+        "modes": {
+            // "grab": {
+            //     "distance": 400,
+            //     "line_linked": {
+            //         "opacity": 1
+            //     }
+            // },
+            "bubble": {
+                "distance": 200,
+                "size": 5,
+                "duration": 2,
+                "opacity": .8,
+                "speed": 3
+            },
+            "repulse": {
+                "distance": 200
+            },
+            // "push": {
+            //     "particles_nb": 4
+            // },
+            // "remove": {
+            //     "particles_nb": 2
+            // }
+        }
+    },
+    "retina_detect": true,
+}
+particlesJS('particles-js', options)
